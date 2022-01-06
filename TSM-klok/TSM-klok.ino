aaa0162afb0c6513fce5c44a8de197a8d864ddad
AmNotOnline <49611956+AmNotOnline@users.noreply.github.com>
2022-01-06 22:22:26 +0100

    Update TSM-klok.ino

--- TSM-klok/TSM-klok.ino
+++ TSM-klok/TSM-klok.ino
@@ -36,8 +36,8 @@ Audio audio;
 Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 void musicStream(String file, byte volume, byte balance) {
-  audio.setVolume(volume);
-  audio.setBalance(balance);
+  audio.setVolume(volume);    // 0..21
+  audio.setBalance(balance);  // -16(L)..(16)(R)
   audio.connecttoFS(SD, file);
   audio.loop();     // loop over de muziekdata, moet in loop?
 }
